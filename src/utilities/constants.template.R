58106130ebe12323ae935eabd83a92574dfe2b90
Mingcen Wei (sAy) <47605337+mingcenwei@users.noreply.github.com>
2021-07-27 04:15:05 +0800

    Add `dataDir`/`exportDir` constants

--- src/utilities/constants.template.R
+++ src/utilities/constants.template.R
@@ -8,6 +8,8 @@ if (!exists("LOCAL_ENVIRONMENT__CONSTANTS_R", mode = "environment")) {
 	LOCAL_ENVIRONMENT__CONSTANTS_R <- new.env()
 
 	projectRoot <- rprojroot::find_rstudio_root_file()
+	dataDir <- file.path(projectRoot, "data")
+	exportDir <- file.path(projectRoot, "export")
 
 	timeZone <- "Asia/Shanghai"
 	# timeZone <- Sys.timezone()
