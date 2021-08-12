a50b7f708151f378eb3ecb7e2d41df0a2aac58d3
Mingcen Wei (sAy) <47605337+mingcenwei@users.noreply.github.com>
2021-08-12 19:04:28 +0800

    Add `timeClustering` (nonvertorized version)

--- src/utilities/helpers.R
+++ src/utilities/helpers.R
@@ -78,4 +78,93 @@ if (!exists("LOCAL_ENVIRONMENT__HELPERS_R", mode = "environment")) {
 					.init = dataFrame
 				)
 		}
+
+	LOCAL_ENVIRONMENT__HELPERS_R$timeClustering_impl <-
+		function(timestamps,
+				 adjacentTolerance,
+				 adjacentWarningTolerance,
+				 totalTolerance,
+				 totalWarningTolerance,
+				 clusterClosenessWarningTolerance) {
+			if (length(timestamps) == 0L) {
+				return(list(clusterIndices = integer(), warnings = character()))
+			}
+
+			currentClusterIndex <- 1L
+			clusterIndices <- c(currentClusterIndex)
+			warnings <- c(list(character()))
+			startTimestamp <- timestamps[1L]
+			previousTimestamp <- timestamps[1L]
+			for (timestamp in timestamps[-1L]) {
+				currentWarnings <- character()
+				if (previousTimestamp + adjacentTolerance >= timestamp &&
+					startTimestamp + totalTolerance >= timestamp) {
+					if (previousTimestamp + adjacentWarningTolerance < timestamp) {
+						currentWarnings <- c(currentWarnings, "Adjacent difference no greater than tolerance, but greater than warning tolerance")
+					}
+					if (startTimestamp + totalWarningTolerance < timestamp) {
+						currentWarnings <- c(currentWarnings, "Total difference no greater than tolerance, but greater than warning tolerance")
+					}
+				} else {
+					if (previousTimestamp + adjacentTolerance >= timestamp) {
+						currentWarnings <- c(currentWarnings, "Adjacent difference no greater than tolerance, but total difference greater than tolerance")
+						if (previousTimestamp + adjacentWarningTolerance < timestamp) {
+							currentWarnings <- c(currentWarnings, "Adjacent difference no greater than tolerance, but greater than warning tolerance")
+						}
+					}
+					if (previousTimestamp + clusterClosenessWarningTolerance >= timestamp) {
+						currentWarnings <- c(currentWarnings, "Clusters closer than warning tolerance")
+					}
+					currentClusterIndex <- currentClusterIndex + 1L
+					startTimestamp <- timestamp
+				}
+				clusterIndices <- c(clusterIndices, currentClusterIndex)
+				warnings <- c(warnings, list(currentWarnings))
+				previousTimestamp <- timestamp
+			}
+			return(list(
+				clusterIndices = clusterIndices,
+				warnings = warnings
+			))
+		}
+	timeClustering <-
+		function(timestamps,
+				 adjacentTolerance = lubridate::minutes(5L),
+				 adjacentWarningTolerance = lubridate::minutes(2L),
+				 totalTolerance = lubridate::minutes(30L),
+				 totalWarningTolerance = lubridate::minutes(5L),
+				 clusterClosenessWarningTolerance = lubridate::minutes(30L)) {
+			timestamps <- as.POSIXct(timestamps)
+			timestampOrder <- order(timestamps, na.last = TRUE)
+			timestampRank <-
+				rank(timestamps, na.last = TRUE, ties.method = "first")
+			sortedNonNaTimestamps <-
+				timestamps[timestampOrder] %>% discard(anyNA)
+
+			c(clusterIndices, warnings) %<-%
+				LOCAL_ENVIRONMENT__HELPERS_R$timeClustering_impl(
+					timestamps = sortedNonNaTimestamps,
+					adjacentTolerance = adjacentTolerance,
+					adjacentWarningTolerance = adjacentWarningTolerance,
+					totalTolerance = totalTolerance,
+					totalWarningTolerance = totalWarningTolerance,
+					clusterClosenessWarningTolerance = clusterClosenessWarningTolerance
+				)
+			clusterIndices <-
+				c(clusterIndices,
+				  rep_len(
+				  	NA_integer_,
+				  	length(timestamps) - length(sortedNonNaTimestamps)
+				  ))
+			warnings <-
+				c(warnings,
+				  rep_len(
+				  	list(character()),
+				  	length(timestamps) - length(sortedNonNaTimestamps)
+				  ))
+			return(list(
+				clusterIndices = clusterIndices[timestampRank],
+				warnings = warnings[timestampRank]
+			))
+		}
 }
