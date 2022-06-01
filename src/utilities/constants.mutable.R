ac54c4d5ebb58ad4ffa7478d13bd72e99da4d355
Mingcen Wei (sAy) <47605337+mingcenwei@users.noreply.github.com>
2022-06-01 16:52:53 +0800

    Update names

--- src/utilities/constants.mutable.R
+++ src/utilities/constants.mutable.R
@@ -7,9 +7,9 @@ library(tidyverse)
 if (!exists("LOCAL_ENVIRONMENT__CONSTANTS_R", mode = "environment")) {
 	LOCAL_ENVIRONMENT__CONSTANTS_R <- new.env()
 
-	projectRoot <- rprojroot::find_rstudio_root_file()
-	dataDir <- file.path(projectRoot, "data")
-	exportDir <- file.path(projectRoot, "export")
+	dir_projectRoot <- rprojroot::find_rstudio_root_file()
+	dir_data <- file.path(dir_projectRoot, "data")
+	dir_export <- file.path(dir_projectRoot, "export")
 
 	timeZone <- "Asia/Shanghai"
 	# timeZone <- Sys.timezone()
--- src/utilities/index.R
+++ src/utilities/index.R
@@ -7,10 +7,10 @@ library(tidyverse)
 if (!exists("LOCAL_ENVIRONMENT__INDEX_R", mode = "environment")) {
 	LOCAL_ENVIRONMENT__INDEX_R <- new.env()
 
-	LOCAL_ENVIRONMENT__INDEX_R$utilitiesDir <- "src/utilities"
+	LOCAL_ENVIRONMENT__INDEX_R$dir_utilities <- "src/utilities"
 
 	list.files(
-		file.path(rprojroot::find_rstudio_root_file(), LOCAL_ENVIRONMENT__INDEX_R$utilitiesDir),
+		file.path(rprojroot::find_rstudio_root_file(), LOCAL_ENVIRONMENT__INDEX_R$dir_utilities),
 		pattern = r"(.+\.R$)",
 		full.names = TRUE
 	) %>%
