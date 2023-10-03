c1bf585277e3254c09171202078b5f696b1c18ad
Mingcen Wei (sAy) <47605337+mingcenwei@users.noreply.github.com>
2023-10-02 22:18:53 -0500

    Add `set_up_r_utilities.fish`

---
+++ set_up_r_utilities.fish
@@ -0,0 +1,23 @@
+#!/usr/bin/env fish
+
+set --global dir_project (realpath -- (status dirname))
+
+function set_up_r_utilities --description "Set up R utilities for the directories" --inherit-variable 'dir_project'
+	for dir in $argv
+		mkdir --parents -- "$dir"/{src/{utilities,data-{analysis,preprocessing}},data,export}
+		cp --interactive -- {$dir_project,$dir}'/src/data-analysis/template.Rmd'
+		cp --interactive -- {$dir_project,$dir}'/src/data-preprocessing/template.Rmd'
+		ln --symbolic --interactive --target-directory="$dir/src/utilities/" -- "$dir_project/src/utilities"/*
+		for file in "$dir/src/utilities"/*.mutable.R
+			set --local file_dereferenced (realpath -- "$file")
+			rm -- "$file"
+			cp -- "$file_dereferenced" "$file"
+		end
+	end
+end
+
+if test 0 -ne (count $argv)
+	set_up_r_utilities $argv
+else
+	set_up_r_utilities (pwd)
+end
