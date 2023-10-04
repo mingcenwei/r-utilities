b870df7242acd69bfc7725ec72aaeff758d587bb
Mingcen Wei (sAy) <47605337+mingcenwei@users.noreply.github.com>
2023-10-04 01:27:39 -0500

    Copy '.gitignore'

--- set_up_r_utilities.fish
+++ set_up_r_utilities.fish
@@ -4,6 +4,7 @@ set --global dir_project (realpath -- (status dirname))
 
 function set_up_r_utilities --description "Set up R utilities for the directories" --inherit-variable 'dir_project'
 	for dir in $argv
+		cp --interactive -- {$dir_project,$dir}'/.gitignore'
 		mkdir --parents -- "$dir"/{src/{utilities,data-{analysis,preprocessing}},data,export}
 		cp --interactive -- {$dir_project,$dir}'/src/data-analysis/template.Rmd'
 		cp --interactive -- {$dir_project,$dir}'/src/data-preprocessing/template.Rmd'
